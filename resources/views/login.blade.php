c8c0132145c631633ad847d80f0dd7aff54d8f1f
Rakantamir <rakantrengganatamir@smkwikrama.sch.id>
2024-03-08 13:53:32 +0700

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,15 @@
+{{-- <div class="login-form">
+    <h2>Login</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <div class="form-group">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <button type="submit">Login</button>
+    </form>
+</div>
+</main> --}}
